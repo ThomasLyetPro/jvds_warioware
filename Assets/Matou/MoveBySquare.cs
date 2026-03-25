d79e2173ad60629df54828f941243c059f953e44
HAMEL Matthieu <MHL9@dsone.3ds.com>
2026-03-25 13:45:35 +0100

    (matou) First commit

---
+++ Assets/Matou/MoveBySquare.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class MoveBySquare : MonoBehaviour
+{
+  public InputAction _pressAction;
+  public int _DirectionFactor = 1; // -1 == goes to the left, 1 to the right
+  private Rigidbody2D _RigidBody = null;
+  
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+    _RigidBody = GetComponent<Rigidbody2D>();
+  }
+
+  void OnEnable()
+  {
+    _pressAction.Enable();
+    //m_Player.Enable();                                // Enable all actions within map.
+  }
+
+  void Update()
+  {
+    if (_pressAction.WasPerformedThisFrame())
+    {
+      Debug.Log("GO GO GO\n");
+      _RigidBody.linearVelocity += new Vector2(400.0f*Time.deltaTime*(float)_DirectionFactor, 0);
+    }
+  }
+}
